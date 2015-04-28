843371bcec6ec06c9bb72367ced3a2c308d28940
Zen Charalambous <Zen.Charalambous@acer.edu.au>
2015-04-28 16:03:01 +1000

    update view to use templates in layoutes like vanilla lithium

--- template/View.php
+++ template/View.php
@@ -3,15 +3,20 @@
 namespace li3_twig\template;
 
 class View extends \lithium\template\View {
-	protected $_processes = array(
-		'all' => array('template'),
-		'template' => array('template'),
-		'element' => array('element')
-	);
-	protected $_steps = array(
-		'template' => array('path' => 'template'),
-		'element' => array('path' => 'element')
-	);
+  protected $_processes = array(
+    'all' => array('template', 'layout'),
+    'template' => array('template'),
+    'element' => array('element')
+  );
+  protected $_steps = array(
+    'template' => array('path' => 'template', 'capture' => array('context' => 'content')),
+    'layout' => array(
+      'path' => 'layout', 'conditions' => 'layout', 'multi' => true, 'capture' => array(
+        'context' => 'content'
+      )
+    ),
+    'element' => array('path' => 'element')
+  );
 }
 
 ?>
